7ed50220bb24c8656369c04fb7f256920f8ae84c
Rinze-CodeGorilla <32885446+Rinze-CodeGorilla@users.noreply.github.com>
2023-06-29 12:56:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Meerdere Scanners
+
+Dit is een voorbeeld van hoe wanneer je meerdere Scanner instanties hebt, dezen elkaar in de weg kunnen zitten, vooral wanneer je meer input hebt dan in 1 keer gelezen wordt, bijvoorbeeld wanneer je meerdere invoeren van een regel leest of wanneer de invoer uit een redirect komt
+
+Je kunt dit programma uitvoeren met
+`java test-input.java`
+
+of met
+`java test-input.java < inputs.txt`
+
---
+++ test-scanners.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class testInput {
+	public static void main(String[] args) {
+		var s1 = new Scanner(System.in);
+		var s2 = s1;
+
+		for (int i = 0; i < 4; i++) {
+			var s1Has = s1.hasNextInt();
+			System.out.println("s1 has int: " + s1Has);
+			if (s1Has) System.out.println("s1: " + s1.nextInt());
+
+			var s2Has = s2.hasNextInt();
+			System.out.println("s2 has int: " + s2Has);
+			if (s2Has) System.out.println("s2: " + s2.nextInt());
+		}
+	}
+}
